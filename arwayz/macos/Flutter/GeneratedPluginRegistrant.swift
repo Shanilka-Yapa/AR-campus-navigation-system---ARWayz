ee71474ad3e25721c9f39b06f9fc7758a4685d65
Shanilka T. Yapa <shanilka.thulshani@gmail.com>
2026-02-22 09:03:12 +0530

    Revert "Merge pull request #4 from Shanilka-Yapa/Hirushi-Navigation"
    This reverts commit 925ccf9186813b068a1cf8436385106bb931bd28, reversing
    changes made to b416bea89fc055c599d31d925f908aea3cd9f817.

--- arwayz/macos/Flutter/GeneratedPluginRegistrant.swift
+++ arwayz/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import file_selector_macos
 import geolocator_apple
 import mobile_scanner
+import package_info_plus
 import share_plus
 import url_launcher_macos
 
@@ -15,6 +16,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
