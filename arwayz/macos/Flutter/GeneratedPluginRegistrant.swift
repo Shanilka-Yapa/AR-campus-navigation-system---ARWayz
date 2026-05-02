e1d240965acdb1c042e1416d74b9ede88aeb4470
Helani Ambalangodage <helaniuambalangodage@gmail.com>
2026-05-02 20:48:30 +0530

    create path preview and update the ar arrow implementation

--- arwayz/macos/Flutter/GeneratedPluginRegistrant.swift
+++ arwayz/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import file_selector_macos
 import firebase_core
 import geolocator_apple
 import mobile_scanner
-import path_provider_foundation
 import share_plus
 import url_launcher_macos
 
@@ -20,7 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
